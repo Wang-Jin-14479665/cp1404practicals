b465ca04fbbb24af470dba16e7a6e796c7743f68
Wang Jin <2605701478@qq.com>
2024-06-02 17:58:32 +0800

    Complete code

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,24 +1,51 @@
+
+
 MENU = "(G)et a valid score \n (P)rint result \n (S)how stars \n (Q)uit"
 
 def main():
+    score = None
     print(MENU)
     choice = input(">>>").upper()
     while choice != "Q":
         if choice == "G":
-            validate_score()
+            score = validate_score()
+
         elif choice == "P":
-            pass
+            if score is not None:
+                result = evaluate_score(score)
+                print(result)
+            else:
+                print("No score available. Please get a valid score first.")
+
         elif choice == "S":
-            pass
+            if score is None:
+                print("No score available. Please get a valid score first.")
+            else:
+                print("*" * score)
+
         else:
             print("Invalid choice")
         print(MENU)
         choice = input(">>>").upper()
+    print("Bye!")
+
 
 def validate_score():
+    """Verify that the scores entered are within the range"""
     score = int(input("Enter score:"))
     while score < 0 or score > 100:
-        print("Invalid score")
+        print("Invalid score. Score must be between 0 and 100.")
         score = int(input("Enter score:"))
+    return score
+
+def evaluate_score(score):
+    """The result is given according to the interval of achievement"""
+    if score > 90:
+        return "Excellent"
+    elif score > 50:
+        return "Passable"
+    else:
+        return "Bad"
+
 
 main()
