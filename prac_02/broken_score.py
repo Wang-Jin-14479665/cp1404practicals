87786735e983c54e6c22d995a10932ea105c848f
Wang Jin <2605701478@qq.com>
2024-06-02 17:06:43 +0800

    Use function

--- prac_02/broken_score.py
+++ prac_02/broken_score.py
@@ -3,49 +3,20 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
+def main():
+    score = float(input("Enter score: "))
+    result = evaluate_score(score)
+    print(result)
 
-# score = float(input("Enter score: "))
-# if score < 0:
-#     print("Invalid score")
-# else:
-#     if score > 100:
-#         print("Invalid score")
-#     if score > 50:
-#         print("Passable")
-#     if score > 90:
-#     print("Excellent")
-# if score < 50:
-#     print("Bad")
-
-# Error 1: Line 17 print statement indent error
-# Corrected code:
-
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-        print("Excellent")
-if score < 50:
-    print("Bad")
-
-# At this point, the code is ready to run, but the partition of the decision structure is still unclear,
-# and the code can also be updated as follows:
-
-score = float(input("Enter score: "))
-
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score > 90:
-        print("Excellent")
-    elif score > 50:
-        print("Passable")
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
     else:
-        print("Bad")
+        if score > 90:
+            return "Excellent"
+        elif score > 50:
+            return "Passable"
+        else:
+            return "Bad"
 
+main()
