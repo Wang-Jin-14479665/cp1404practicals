339c4acc0e4f53e4a28faeb920705c02a5086258
Wang Jin <2605701478@qq.com>
2024-06-02 17:12:37 +0800

    Add functionality - Validate function functionality using random numbers

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,11 +2,17 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+
 
 def main():
     score = float(input("Enter score: "))
     result = evaluate_score(score)
     print(result)
+    new_score = random.randint(1, 100)
+    new_result = evaluate_score(new_score)
+    print(new_result)
+
 
 def evaluate_score(score):
     if score < 0 or score > 100:
@@ -18,5 +24,6 @@ def evaluate_score(score):
             return "Passable"
         else:
             return "Bad"
+
 
 main()
