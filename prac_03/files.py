1f63b68393428b8ed3cb5efb23af8982d243870f
Wang Jin <2605701478@qq.com>
2024-06-08 18:24:14 +0800

    Task 2: Read the name from name.txt and print a greeting

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,7 +1,11 @@
 name = input("Enter a nane: ")
-
 file = open("name.txt", "w")
-
 file.write(name)
-
 file.close()
+
+# Task 2: Read the name from name.txt and print a greeting
+file = open('name.txt', 'r')
+name_from_file = file.read()
+file.close()
+
+print(f"Hi {name_from_file}!")
