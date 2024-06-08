bedc03fab1f392670e52ba811644048896d3a5c8
Wang Jin <2605701478@qq.com>
2024-06-08 17:38:08 +0800

    Count each kind of character

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -36,9 +36,17 @@ def is_valid_password(password):
     number_of_upper = 0
     number_of_digit = 0
     number_of_special = 0
+    # TODO: count each kind of character (use str methods like isdigit)
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if character.islower():
+            number_of_lower += 1
+        if character.isupper():
+            number_of_upper += 1
+        if character.isdigit():
+            number_of_digit += 1
+        if character in SPECIAL_CHARACTERS:
+            number_of_special += 1
+
 
     # TODO: if any of the 'normal' counts are zero, return False
 
