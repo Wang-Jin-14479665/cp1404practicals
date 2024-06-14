4bb0d1e8f06e49d4a79442483e014262e19ab12c
Wang Jin <2605701478@qq.com>
2024-06-15 00:58:24 +0800

    refactor (move) the report printing into its own function.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -14,12 +14,16 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_report(numbers_of_month, incomes)
+
+
+def print_report(numbers_of_month, incomes):
+    """This function will print report"""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, numbers_of_month + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
-
 
 main()
