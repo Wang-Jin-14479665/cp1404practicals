20074caedd2f95ec1ca77c69c8301fd6ea0adde0
Wang Jin <2605701478@qq.com>
2024-06-16 14:21:46 +0800

    Revert "Basic list operations."
    This reverts commit e715cad2

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
+# 初始化一个空列表来存储数字
+numbers = []
 
-"""
-CP1404/CP5632 - Practical
-Fill in the TODOs to complete the task
-"""
+# 提示用户输入 5 个数字
+for i in range(5):
+    number = float(input("Number: "))
+    numbers.append(number)
 
-is_finished = False
-while not is_finished:
-    try:
-        result = int(input("Enter a valid integer: "))
-        is_finished = True  # TODO: Set is_finished to True to break the loop
-    except ValueError:  # TODO: Catch ValueError exception for invalid integer input
-        print("Please enter a valid integer.")
-print("Valid result is:", result)
+# 输出数字的相关信息
+print(f"The first number is {int(numbers[0])}")
+print(f"The last number is {int(numbers[-1])}")
+print(f"The smallest number is {int(min(numbers))}")
+print(f"The largest number is {int(max(numbers))}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
