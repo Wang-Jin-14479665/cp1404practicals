fae8d664d85825aa6bdeb2f0cdc4e6ce4e96cc06
Wang Jin <2605701478@qq.com>
2024-06-22 19:40:51 +0800

    Complete prac_5 emails exercise.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,22 +8,25 @@ Actual:   minutes
 def main():
     emails = {}
 
-    user_email = input('Name: ')
+    user_email = input('Email: ').strip()
     while user_email != "":
-        name = extract_name()  # A function that extracts the first half of the name from the email
-        name_check = input(f"Is your name{name}? (Y/n):").upper()
+        name = extract_name(user_email)  # A function that extracts the first half of the name from the email
+        name_check = input(f"Is your name {name}? (Y/n): ").upper()
         if name_check not in ("Y", "YES", ""):
             name = input("Name: ")
 
-        emails[user_email] = name
-        user_email = input('Name: ')
+        emails[name] = user_email
+        user_email = input('Email: ').strip()
 
     for name, email in emails.items():
         print(f"{name} ({email})")
 
 
-def extract_name():
+def extract_name(user_email):
     """This function extracts the name of the first half of the entered email"""
+    parts = user_email.split("@")[0].split(".")
+    name = " ".join(parts).title()
+    return name
 
 
 main()
