22b0f06cefdd5b9cbab2124d299827a978bc27a3
Wang Jin <2605701478@qq.com>
2024-06-22 23:07:18 +0800

    Printed result part is summarized as a function so that main function is neater.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,24 +7,21 @@ import csv
 FILE_PATH = 'wimbledon.csv'
 
 def main():
+    data = process_data()
+    champions = process_champions(data)
+    countries = process_countries(data)
+    display_champions(champions)
+    display_countries(countries)
+
+def process_data():
+    """Use the imported csv processing file"""
     data = []
     with open(FILE_PATH, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
         header = next(reader)
         for row in reader:
             data.append(row)
-    # print(data)
-
-    champions = process_champions(data)
-    countries = process_countries(data)
-    print(countries)
-
-    print("Wimbledon Champions: ")
-    for champion, count in champions.items():
-        print(f"{champion} {count}")
-
-    print(f"These {len(countries)} countries have won Wimbledon: ")
-    print(" ".join(countries))
+    return data
 
 def process_champions(data):
     """Extract all champion names and number of championships from the data"""
@@ -43,5 +40,16 @@ def process_countries(data):
     for row in data:
         countries.add(row[1])
     return sorted(countries)
+
+def display_champions(champions):
+    """Output processed championship results on request"""
+    print("Wimbledon Champions: ")
+    for champion, count in champions.items():
+        print(f"{champion} {count}")
+
+def display_countries(countries):
+    """Export all winning countries according to the requirements and statistics"""
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(" ".join(countries))
 
 main()
