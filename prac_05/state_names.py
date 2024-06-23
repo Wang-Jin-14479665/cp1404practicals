b462331d29c1380366819a4e3c7ff499d7f44ec9
Wang Jin <2605701478@qq.com>
2024-06-23 15:44:11 +0800

    state name

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,7 @@ State names in a dictionary
 File needs reformatting
 """
 
-<<<<<<< HEAD
+
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(CODE_TO_NAME)
@@ -12,13 +12,12 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 for code, name in CODE_TO_NAME.items():
     print(f"{code:<3} is {name}")
 
-=======
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
->>>>>>> 913b2aa (Now typing lowercase letters can also work)
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     try:
