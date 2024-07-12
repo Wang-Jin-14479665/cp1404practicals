afc115395acb9e8b85e24d1d70c80555cd89ddeb
Wang Jin <2605701478@qq.com>
2024-07-12 12:59:33 +0800

    car and used_cars completed

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -14,5 +14,15 @@ def main():
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
+    # Modifications
+    limo = Car(100, "limo")
+    limo.add_fuel(20)  # Add 20 more units of fuel to this new car object using the add method.
 
+    # Print the amount of fuel in the car.
+    print(f"Limo has fuel: {limo.fuel}")
+
+    # Attempt to drive the car 115 km using the drive method.
+    limo.drive(115)
+
+    print(limo)
 main()
