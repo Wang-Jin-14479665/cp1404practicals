516d8976dd61d8555d8742e21cdae1547267eece
Wang Jin <2605701478@qq.com>
2024-07-14 12:41:07 +0800

    guitar finished

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -22,28 +22,3 @@ class Guitar:
         return self.get_age() >= 50
 
 
-def main():
-    guitars = []
-
-    print("My guitars!")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-        print(f"{guitar} added.\n")
-        name = input("Name: ")
-
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-    guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
-
-    print("\n... snip ...\n")
-    print("These are my guitars:")
-    for i, guitar in enumerate(guitars, 1):
-        vintage_string = "(vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
-
-
-main()
