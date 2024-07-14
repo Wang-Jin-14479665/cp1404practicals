f9b95c0b3ca4c8cd90c9edfbb89ceb0e19abe2dc
Wang Jin <2605701478@qq.com>
2024-07-14 11:58:43 +0800

    guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,7 +1,7 @@
 """
 guitar
 Estimated time: 30 minutes
-Actual time:  minutes
+Actual time: 20 minutes
 """
 CURRENT_YEAR = 2024
 
@@ -19,3 +19,28 @@ class Guitar:
 
     def is_vintage(self):
         return self.get_age() >= 50
+
+def main():
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Fender Stratocaster", 2014, 765.4))
+
+    print("\n... snip ...\n")
+    print("These are my guitars:")
+    for i,guitar in enumerate(guitars,1):
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name} ({guitar.year}), worth ${guitar.cost}{vintage_string}")
+
+main()
